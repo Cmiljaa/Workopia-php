b95e0707f71bb6e23635c93b121f9484cf7695a9
Cmiljaa <147316652+Cmiljaa@users.noreply.github.com>
2024-06-24 23:50:44 +0200

    Added Database class

--- Database.php
+++ Database.php
@@ -12,14 +12,36 @@ class Database{
 
      public function __construct($config)
      {
-        $dns = "mysql:host={config['host]};port={config['port']};dbname={config['dbname']};";
+        $dns = "mysql:host={$config['host']};port={$config['port']};dbname={$config['dbname']};";
 
-        $options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ];
 
         try{
-            $this->conn = new PDO($dns, $config['username'], $config['password']);
+            $this->conn = new PDO($dns, $config['username'], $config['password'], $options);
         }catch(PDOException $e){
             echo "Database is not connected: " . $e->getMessage();
+        }
+     }
+
+     /**
+     * Query the database
+     * 
+     * @param string $query
+     * 
+     * @return PDOstatement
+     * @throws PDOException
+     */
+
+     public function query($query){
+        try{
+            $stmt = $this->conn->prepare($query);
+            $stmt -> execute();
+            return $stmt -> fetchAll();
+        }catch(PDOException $e){
+            echo "Query failed to execute: " . $e -> getMessage();
         }
      }
 }
